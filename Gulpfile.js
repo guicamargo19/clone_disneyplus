6e484e8d01c002c7e5b388ca9215011c4ad777bf
camargo <gui.ferreira.camargo@gmail.com>
2023-12-19 14:52:06 -0300

    Hero Section

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require("gulp-imagemin");
 
 function styles() {
     return gulp.src("./src/styles/*.scss")
@@ -7,8 +8,14 @@ function styles() {
     .pipe(gulp.dest("./dist/css"));
 }
 
+function images() {
+    return gulp.src("./src/images/**/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("./dist/images"));
+}
 
-exports.default = styles;
+exports.default = gulp.parallel(styles, images);
+
 exports.watch = function() {
     gulp.watch("./src/styles/*.scss", gulp.parallel(styles));
 }
