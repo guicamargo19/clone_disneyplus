2f0b0bb98c067dc591e4b8b7c4eafa2520a498aa
camargo <gui.ferreira.camargo@gmail.com>
2023-12-20 15:43:26 -0300

    Adicionado header e footer

--- src/main.js
+++ src/main.js
@@ -2,6 +2,20 @@ document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
 
+    const heroSection = document.querySelector(".hero");
+    const heroHeight = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const positionWindow = window.scrollY;
+
+        if (positionWindow < heroHeight) {
+            hideHeaderElements();
+        } else {
+            showHeaderElements();
+        }
+    })
+
+    // Programação das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(button) {
             const target = button.target.dataset.tabButton;
@@ -13,10 +27,21 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }
 
+    // Section FAQ, Accordion
     for (let i = 0; i<questions.length; i++) {
         questions[i].addEventListener('click', openOrCloseAnswer);
     }
 });
+
+function showHeaderElements() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
+function hideHeaderElements() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
 
 function openOrCloseAnswer(elemento) {
     const classe = 'faq__questions__item--is-open';
